0cdadfbe93341d30a3b248bf6b477bcf36a43640
Ankit Jain <akjain6067.aj@gmail.com>
2021-01-11 17:34:08 +0530

    110. Image not loading fixed

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,7 +4,6 @@ class ImageCard extends React.Component {
   state = {};
 
   render() {
-    console.log(this.props.image);
     const { urls, alt_description } = this.props.image;
     return <img src={urls.thumb} alt={alt_description}></img>;
   }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,13 +2,11 @@ import React from "react";
 import ImageCard from "./ImageCard";
 // import "./../styles/ImageList.css";
 class ImageList extends React.Component {
-  state = {};
-  images = this.props.images.map((image) => {
-    return <ImageCard image={image} key={image.id}></ImageCard>;
-  });
-
   render() {
-    return <div>{this.images}</div>;
+    const images = this.props.images.map((image) => {
+      return <ImageCard image={image} key={image.id} />;
+    });
+    return <div>{images}</div>;
   }
 }
 
